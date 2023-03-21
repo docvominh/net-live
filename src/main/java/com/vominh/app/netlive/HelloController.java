d4720f0840bfe74a7289e4479b90a0863abc8c84
Minh Pham <phamducminh1990@gmail.com>
2023-03-21 22:59:56 +0700

    init project

---
+++ src/main/java/com/vominh/app/netlive/HelloController.java
@@ -0,0 +1,14 @@
+package com.vominh.app.netlive;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
